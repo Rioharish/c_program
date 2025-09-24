fdba978c17db97cc8e2c5e1e31debc7ad02d4847
Rioharish <rioharish1330@gmail.com>
2025-09-24 14:41:05 +0530

    Add files via upload

---
+++ harish's file q1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int num1,num2;
+	printf("Enter two number:");
+	scanf("%d%d",&num1,&num2);
+	if(num1>num2){
+		printf("Greatest number is %d\n",num1);
+	} else if(num2>num1){
+		printf("Greatest number is %d\n",num2);
+	}else{
+		printf("both numbers are equal.\n");
+	}
+	return 0;
+}
